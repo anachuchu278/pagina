26d8c96dedc528f3c572765140011ad35e1b8fe7
Cristian <cristian.pacman3@gmail.com>
2024-06-27 08:37:15 -0300

    Validacion fecha de turno 27/06/2024

--- app/Views/TurnoNew.php
+++ app/Views/TurnoNew.php
@@ -9,7 +9,16 @@
     <form action="newTurno1" method="POST">
 
         <label for="fecha">Fecha y hora:</label><br>
-        <input type="datetime-local" id="fecha_hora" name="fecha_hora" required><br>
+
+        <?php
+            $mindate = date("Y-m-d");
+            $mintime = date("h:i");
+            $min = $mindate."T".$mintime;
+            $maxdate = date("Y-m-d", strtotime("+10 Days"));
+            $maxtime = date("h:i");
+            $max = $maxdate."T".$maxtime;
+        ?>
+        <input type="datetime-local" id="fecha_hora" min="<?php echo $min;?>" max="<?php echo $max;?>" name="fecha_hora" required><br>
 
         <input type="submit" value="Añadir Turno">
     </form>
