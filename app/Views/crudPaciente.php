2ac69592324d434052d4bdd1129abb55a1099c74
mateo <mateobargas@alumnos.itr3.edu.ar>
2024-08-20 08:17:50 -0300

    css vista turnos y datos. 20/08

--- app/Views/crudPaciente.php
+++ app/Views/crudPaciente.php
@@ -2,7 +2,7 @@
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <link rel="stylesheet" href="<?= base_url('css/crud.css')?>">
+    <link rel="stylesheet" href="<?= base_url('css/crudPaciente.css')?>">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Registro de pacientes</title>
 </head>
