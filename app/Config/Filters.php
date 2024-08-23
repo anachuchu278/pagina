db7beecb6a78deecc322fd76f5eb134745aa7ef9
mateo <mateobargas@alumnos.itr3.edu.ar>
2024-08-23 10:36:32 -0300

    calendario 23/8

--- app/Config/Filters.php
+++ app/Config/Filters.php
@@ -105,11 +105,11 @@ class Filters extends BaseFilters
      *
      * @var array<string, array<string, list<string>>>
      */
-    public array $filters = [ 
-        "SessionAdmin" => [
-            "before" => [
-                "vistaAdmin"
-            ]
-        ] 
-    ]; 
+    //public array $filters = [ 
+       // "SessionAdmin" => [
+         //   "before" => [
+           //     "vistaAdmin"
+            //]
+        //] 
+    //]; 
 }
--- app/Views/calendario.php
+++ app/Views/calendario.php
@@ -7,46 +7,45 @@
     <title>Calendario</title>
     <link rel="stylesheet" href="<?php echo base_url('css/calendario.css')?>">
 </head>
-<body>
-    <div class="root">
-        <div class="calendar" id="calendar">
-            <div clas="calendar_header">
-                <button class="control control--prev">&lt;
-                </button>
-                <span class="month-name">Agosto 2024</span>
-                <button class="control control--next">&gt;</button>
+<body class="light">
+    <div class="calendar">
+        <div class="calendar-header">
+            <span class="month-picker" id="month-picker">
+                Agosto
+            </span>
+            <div class="year-picker">
+                <span class="year-change" id="prev-year">
+                    <pre><</pre>
+                </span>
+                <span id="year">2024</span>
+                <span class="year-change" id="next-year">
+                    <pre>></pre>
+                </span>
             </div>
         </div>
-        <div class="calendar_body" id="calendar2">
-            <div class="grid">
-                <div class="grid_header">
-                    <div class="grid__body">
-                        <span class="grid__cell grid_cell--gh">Lun</span>
-                        <span class="grid__cell grid_cell--gh">Mar</span>
-                        <span class="grid__cell grid_cell--gh">Mie</span>
-                        <span class="grid__cell grid_cell--gh">Jue</span>
-                        <span class="grid__cell grid_cell--gh">Vie</span>
-                        <span class="grid__cell grid_cell--gh">Sab</span>
-                        <span class="grid__cell grid_cell--gh">Dom</span>
-                    </div>
-                </div>
+        <div class="calendar-body">
+            <div class="calendar-week-days">
+                <span class="week-day">Lun</span>
+                <span class="week-day">Mar</span>
+                <span class="week-day">Mie</span>
+                <span class="week-day">Jue</span>
+                <span class="week-day">Vie</span>
+                <span class="week-day">Sab</span>
+                <span class="week-day">Dom</span>
+            </div>
+            <div class="calendar-days">
+                <div>1</div>
+                <div>2</div>
+                <div>3</div>
+                <div>4</div>
+                <div>5</div>
+                <div>6</div>
+                <div>7</div>
+
             </div>
 
         </div>
     </div>
-    <script  type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/moment.js/2.24.0/moment.min.js"></script>
-    <script  type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/moment.js/2.24.0/locale/es.js"></script>
-    <script  type="text/javascript" src="<?php base_url('script.js')?>"></script>
-    <script type="text/javascript">
-        let calendar = new Calendar('calendar');
-        calendar.getElement().addEventListener('change', e => {
-            console.log(calendar.value().format('LLL'));
-        });
-
-        let calendar2 = new Calendar('calendar2');
-        calendar2.getElement().addEventListener('change', e => {
-            console.log(calendar2.value().format('LLL'));
-        });
-    </script>
+ 
 </body>
 </html>
