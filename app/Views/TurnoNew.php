f4ee47500ef780c8962c26ce10c13f5bc410aa50
Cristian <cristian.pacman3@gmail.com>
2024-09-19 14:12:23 -0300

    CSS 19/09/2024

--- app/Views/TurnoNew.php
+++ app/Views/TurnoNew.php
@@ -1,14 +1,14 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <link rel="stylesheet" href="<?php echo base_url('css/turno.css')?>">
+    <link rel="stylesheet" href="<?php echo base_url('css/editPaciente.css')?>">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Nuevo Turno</title>
 </head>
 <body>
-    <a href="<?php echo base_url('pagina') ?>" class="boton-volver">Volver</a>
+    <!-- <a href="<?php echo base_url('pagina') ?>" class="boton-volver">Volver</a> -->
     <form action="newTurno1" method="POST">
         <label for="id_Medico">Medico:</label><br>
         <select class="form-select" name="id_Medico" id="id_Medico" required>
