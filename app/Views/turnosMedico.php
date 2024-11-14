7c7504b0e7758532e75a7610ddec35f46daf6457
mateo <mateobargas@alumnos.itr3.edu.ar>
2024-11-14 14:35:19 -0300

    TURNOS DE MEDICO

--- app/Views/turnosMedico.php
+++ app/Views/turnosMedico.php
@@ -1,25 +1,25 @@
-
+<head>
+    <title>Turnos del Medico</title>
+    <link rel="stylesheet" href="<?= base_url('css/turnosMedico.css')?>">
+</head>
 <h1>Turnos del Médico</h1>
 
 <?php foreach ($turnosPorDia as $dia => $turnos): ?>
-    <?php var_dump($turnos); ?>
-    <h2><?= date('d/m/Y', strtotime($dia)) ?> - <?= $diasSemana[date('N', strtotime($dia))] ?></h2>
+    <h2><?= date('d/m/Y') ?> - <?= $diasSemana[date('N')] ?></h2>
     <table class="table">
         <thead>
             <tr>
-                <th>Hora del Turno</th>
-                <th>Horario del Médico</th>
                 <th>Paciente</th>
                 <th>Estado</th>
+                <th>Hora del Turno</th>
             </tr>
         </thead>
         <tbody>
             <?php foreach ($turnos as $turno): ?>
                 <tr>
-                    <td><?= date('H:i', strtotime($turno['id_Horario'])) ?></td>
-                    
                     <td><?= esc($turno['Paciente']) ?></td>
                     <td><?= esc($turno['estado']) ?></td>
+                    <td><?= esc($turno['fecha_turno'])?></td>
                 </tr>
             <?php endforeach; ?>
         </tbody>
